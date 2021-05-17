83cbb20052a1dc6c7191bb82c3dd4b6acbf80a06
james46007 <rootkaliente@gmail.com>
2021-05-16 19:06:30 -0500

    Video completado 23

--- ecommerce_rest/apps/products/api/views/product_views.py
+++ ecommerce_rest/apps/products/api/views/product_views.py
@@ -6,11 +6,22 @@ from rest_framework import status
 from apps.base.api import GeneralListAPIView
 from apps.products.api.serializers.product_serializer import ProductSerializer
 
-class ProductListAPIView(GeneralListAPIView):
-    serializer_class = ProductSerializer
+# class ProductListAPIView(GeneralListAPIView):
+#     serializer_class = ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+# class ProductCreateAPIView(generics.CreateAPIView):
+#     serializer_class = ProductSerializer
+
+#     def post(self, request):
+#         serializer = self.serializer_class(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'message':'Producto guardado correctamente!'},status= status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status= status.HTTP_400_BAD_REQUEST)
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
+    queryset = ProductSerializer.Meta.model.objects.filter(state = True)
 
     def post(self, request):
         serializer = self.serializer_class(data = request.data)
